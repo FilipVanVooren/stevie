2284d2a96ed448a8b164747519c4744ddba57bd2
Filip van Vooren <14039781+FilipVanVooren@users.noreply.github.com>
2022-11-13 17:48:48 +0100

    python 3.11

--- build/assemble.sh
+++ build/assemble.sh
@@ -4,7 +4,7 @@
 source helper.sh
 
 # Constants
-IMAGE="${IMAGE:-easyxdt99:3.5.0-cpython3.10-alpine}"
+IMAGE="${IMAGE:-easyxdt99:3.5.0-cpython3.11-alpine}"
 
 # Variables
 marker="***************************************************************"
