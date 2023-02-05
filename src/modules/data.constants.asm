cb7bb0a0bdbd8ad7c2cfce4b526541314bd064be
Filip van Vooren <14039781+FilipVanVooren@users.noreply.github.com>
2023-02-05 15:54:46 +0100

    Tweaked color schemes

--- src/modules/data.constants.asm
+++ src/modules/data.constants.asm
@@ -22,63 +22,54 @@ nosprite:
 ***************************************************************
 * Stevie color schemes table
 *--------------------------------------------------------------
-* Word 1
-* A  MSB  high-nibble    Foreground color text line in frame buffer
-* B  MSB  low-nibble     Background color text line in frame buffer
-* C  LSB  high-nibble    Foreground color top/bottom line
-* D  LSB  low-nibble     Background color top/bottom line
+* ; Word 1
+* ; A  MSB  high-nibble    Foreground color text line in frame buffer
+* ; B  MSB  low-nibble     Background color text line in frame buffer
+* ; C  LSB  high-nibble    Foreground color top/bottom line
+* ; D  LSB  low-nibble     Background color top/bottom line
+* ; Word 2
+* ; E  MSB  high-nibble    Foreground color cmdb pane
+* ; F  MSB  low-nibble     Background color cmdb pane
+* ; G  LSB  high-nibble    Cursor foreground color cmdb pane
+* ; H  LSB  low-nibble     Cursor foreground color frame buffer
+* ; Word 3
+* ; I  MSB  high-nibble    Foreground color busy top/bottom line
+* ; J  MSB  low-nibble     Background color busy top/bottom line
+* ; K  LSB  high-nibble    Foreground color marked line in frame buffer
+* ; L  LSB  low-nibble     Background color marked line in frame buffer
+* ; Word 4
+* ; M  MSB  high-nibble    Foreground color command buffer header line
+* ; N  MSB  low-nibble     Background color command buffer header line
+* ; O  LSB  high-nibble    Foreground color line+column indicator frame buffer
+* ; P  LSB  low-nibble     Foreground color ruler frame buffer
 *
-* Word 2
-* E  MSB  high-nibble    Foreground color cmdb pane
-* F  MSB  low-nibble     Background color cmdb pane
-* G  LSB  high-nibble    Cursor foreground color cmdb pane
-* H  LSB  low-nibble     Cursor foreground color frame buffer
-*
-* Word 3
-* I  MSB  high-nibble    Foreground color busy top/bottom line
-* J  MSB  low-nibble     Background color busy top/bottom line
-* K  LSB  high-nibble    Foreground color marked line in frame buffer
-* L  LSB  low-nibble     Background color marked line in frame buffer
-*
-* Word 4
-* M  MSB  high-nibble    Foreground color command buffer header line
-* N  MSB  low-nibble     Background color command buffer header line
-* O  LSB  high-nibble    Foreground color line+column indicator frame buffer
-* P  LSB  low-nibble     Foreground color ruler frame buffer
-*
-* Colors
-* 0  Transparant
-* 1  black
-* 2  Green
-* 3  Light Green
-* 4  Blue
-* 5  Light Blue
-* 6  Dark Red
-* 7  Cyan
-* 8  Red
-* 9  Light Red
-* A  Yellow
-* B  Light Yellow
-* C  Dark Green
-* D  Magenta
-* E  Grey
-* F  White
+* ; Colors
+* ; 0  Transparant      8  Red
+* ; 1  black            9  Light Red
+* ; 2  Green            A  Yellow
+* ; 3  Light Green      B  Light Yellow
+* ; 4  Blue             C  Dark Green
+* ; 5  Light Blue       D  Magenta
+* ; 6  Dark Red         E  Grey
+* ; 7  Cyan             F  White
 *--------------------------------------------------------------
-tv.colorscheme.entries   equ 10 ; Entries in table
-
+tv.colorscheme.entries  equ 12        ; Entries in table
 tv.colorscheme.table:
         ;                             ; #
         ;      ABCD  EFGH  IJKL  MNOP ; -
         data  >f417,>f171,>1b1f,>7111 ; 1  White on blue with cyan touch
-        data  >a11a,>f0ff,>1f1a,>f1ff ; 2  Dark yellow on black
-        data  >2112,>f0ff,>1f12,>f1f6 ; 3  Dark green on black
-        data  >f41f,>1e11,>1a17,>1e11 ; 4  White on blue
-        data  >e11e,>e1ff,>1f1e,>e1ff ; 5  Grey on black
-        data  >1771,>1016,>1b71,>1711 ; 6  Black on cyan
-        data  >1ff1,>1011,>f1f1,>1f11 ; 7  Black on white
-        data  >1af1,>a1ff,>1f1f,>f11f ; 8  Black on dark yellow
-        data  >21f0,>21ff,>f112,>21ff ; 9  Dark green on black
-        data  >f5f1,>e1ff,>1b1f,>f131 ; 10 White on light blue
+        data  >21f0,>21ff,>f112,>21ff ; 2  Dark green on black (minimalistic)
+        data  >a11a,>f0ff,>1f1a,>f1ff ; 3  Dark yellow on black
+        data  >1e1e,>1e11,>1ee1,>1e11 ; 4  Black on grey (minimalistic)        
+        data  >151f,>1511,>1f1f,>1511 ; 5  Black on light blue        
+        data  >1313,>1311,>1331,>1311 ; 6  Black on light green (minimalistic)
+        data  >1771,>1011,>0171,>1711 ; 7  Black on cyan        
+        data  >2112,>f0ff,>1f12,>f1f6 ; 8  Dark green on black         
+        data  >1ff1,>1011,>f1f1,>1f11 ; 9  Black on white
+        data  >1af1,>a111,>1f1f,>f11f ; 10 Black on dark yellow
+        data  >1919,>1911,>1991,>1911 ; 11 Black on light red (minimalistic)
+        data  >fdf1,>1d11,>fddf,>1d11 ; 12 White on magenta
+
         even
 
 tv.tabs.table.lr:
