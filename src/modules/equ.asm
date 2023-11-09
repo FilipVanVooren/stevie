d10c42fc10485ff06d43c9c79b2679b5c1cf42ce
Filip van Vooren <14039781+FilipVanVooren@users.noreply.github.com>
2023-11-09 19:37:51 +0100

    File browser WIP

--- src/modules/equ.asm
+++ src/modules/equ.asm
@@ -392,12 +392,14 @@ tv.mc.fname       equ  >dea0           ; Default Master Catalog (80 bytes)
 ; Directory/File catalog               @>e000-efff    (4096 bytes)
 ;-----------------------------------------------------------------
 cat.top           equ  >e000           ; Top of file catalog
-cat.filecount     equ  cat.top
-cat.volname       equ  cat.top + 2     ; Volume name
-cat.fnlist        equ  cat.top + 14    ; Filename list (127*11) 
-cat.ftlist        equ  cat.top + 1412  ; Filetype list (127)
-cat.fslist        equ  cat.top + 1540  ; Filesize size (127)
-cat.rslist        equ  cat.top + 1668  ; Record size list (127)
+cat.filecount     equ  cat.top         ; Total files in catalog
+cat.idxcurrent    equ  cat.top + 2     ; Index of current file in catalog
+cat.1stpage.ptr   equ  cat.top + 4     ; Pointer to 1st file on page in catalog
+cat.volname       equ  cat.top + 6     ; Volume name
+cat.fnlist        equ  cat.top + 18    ; Filename list (127*11) 
+cat.ftlist        equ  cat.top + 1416  ; Filetype list (127)
+cat.fslist        equ  cat.top + 1544  ; Filesize size (127)
+cat.rslist        equ  cat.top + 1672  ; Record size list (127)
 cat.size          equ  3840            ; Catalog total size
 ;-----------------------------------------------------------------
 ; Command buffer                       ; @>ef00-efff   (256 bytes)
--- src/modules/pane.filebrowser.asm
+++ src/modules/pane.filebrowser.asm
@@ -31,59 +31,22 @@ pane.filebrowser:
         mov   @cat.filecount,tmp0   ; Get number of files
         jeq   pane.filebrowser.exit ; Exit if nothing to display
         ;------------------------------------------------------
-        ; Prepare header
-        ;------------------------------------------------------
-        bl    @putat
-              byte 1,1
-              data txt.volume       ; Display "Volume: ...."   
-
-        bl    @putat
-              byte 2,1
-              data txt.filelist     ; Display "Filename Type Size ...."   
-
-        bl    @vchar
-              data >021a                       ; Starting position YX
-              byte >10                         ; Vertical line char
-              byte pane.botrow - cmdb.rows - 2 ; Y-repeat
-              data >0236                       ; Starting position YX
-              byte >10                         ; Vertical line char
-              byte pane.botrow - cmdb.rows - 2 ; Y-repeat
-              data EOL
-        ;------------------------------------------------------
-        ; Show filenames
-        ;------------------------------------------------------
-        bl    @at                   ; Set cursor position
-              byte 3,1              ; Y=2, X=1
-
-        li    tmp1,cat.fnlist       ; Get filename list
-        mov   @cat.filecount,tmp2   ; Number of filenames to display
-
-        mov   @fb.scrrows,tmp0      ; \ Determine cutover row for filename
-        s     @cmdb.scrrows,tmp0    ; | column list and store in MSB of tmp0
-        sla   tmp0,8                ; /
-
-        ori   tmp0,28               ; Set offset for new column in filename list
-
-        bl    @putlst               ; Loop over string list and display
-                                    ; \ i  @wyx = Cursor position
-                                    ; | i  tmp0 = Cutover row and column offset
-                                    ; |           for next column, >0000 for
-                                    ; |           single column list
-                                    ; | i  tmp1 = Pointer to first length-
-                                    ; |           prefixed string in list
-                                    ; / i  tmp2 = Number of strings to display
-        ;------------------------------------------------------
         ; Show volume name, no files, device path
         ;------------------------------------------------------
-        mov   @cat.volname,tmp0     ; Volume name set?
-        jeq   pane.filebrowser.exit ; No, skip
-
         bl    @putat
-              byte 1,9
+              byte 0,0
+              data txt.volume       ; Display "Volume: ...."   
+
+        mov   @cat.volname,tmp0        ; Volume name set?
+        jeq   pane.filebrowser.nofiles ; No, skip
+                                    
+        bl    @putat
+              byte 0,8
               data cat.volname      ; Display volume name
         ;-------------------------------------------------------
-        ; Show number of files
+        ; Show number of files, device path
         ;-------------------------------------------------------
+pane.filebrowser.nofiles:        
         andi  config,>7fff          ; Do not print number
                                     ; (Reset bit 0 in config register)
 
@@ -97,14 +60,55 @@ pane.filebrowser:
               data rambuf,rambuf + 5,32
 
         bl    @putat
-              byte 1,27            
+              byte 0,26
               data rambuf + 5       ; Display number of files
-        ;-------------------------------------------------------
-        ; Show device path
-        ;-------------------------------------------------------
+
         bl    @putat
-              byte 1,41
-              data myfile
+              byte 0,39
+              data myfile           ; Show device path
+        ;------------------------------------------------------
+        ; Draw vertical lines
+        ;------------------------------------------------------
+        bl    @vchar
+              byte 1,19                        ; Starting position YX  
+              byte >10                         ; Vertical line char
+              byte pane.botrow - cmdb.rows - 1 ; Y-repeat
+              byte 1,39                        ; Starting position YX  
+              byte >10                         ; Vertical line char
+              byte pane.botrow - cmdb.rows - 1 ; Y-repeat
+              byte 1,59                        ; Starting position YX  
+              byte >10                         ; Vertical line char
+              byte pane.botrow - cmdb.rows - 1 ; Y-repeat              
+              data EOL
+        ;------------------------------------------------------
+        ; Prepare for displaying filenames
+        ;------------------------------------------------------
+        li    tmp0,cat.fnlist       ; Get filename list
+        mov   tmp0,@cat.1stpage.ptr ; Save pointer
+        ;------------------------------------------------------
+        ; Show filenames
+        ;------------------------------------------------------
+        bl    @at                   ; Set cursor position
+              byte 1,1              ; Y=1, X=1
+
+        mov   @cat.1stpage.ptr,tmp1 ; Pointer to 1st filename to dispay in list
+        mov   @cat.filecount,tmp2   ; Number of filenames to display
+        li    tmp2,64
+
+        mov   @fb.scrrows,tmp0      ; \ Determine cutover row for filename
+        s     @cmdb.scrrows,tmp0    ; | column list and store in MSB of tmp0
+        sla   tmp0,8                ; /
+
+        ori   tmp0,20               ; Set offset for new column in filename list
+
+        bl    @putlst               ; Loop over string list and display
+                                    ; \ i  @wyx = Cursor position
+                                    ; | i  tmp0 = Cutover row and column offset
+                                    ; |           for next column, >0000 for
+                                    ; |           single column list
+                                    ; | i  tmp1 = Pointer to first length-
+                                    ; |           prefixed string in list
+                                    ; / i  tmp2 = Number of strings to display
         ;------------------------------------------------------
         ; Exit
         ;------------------------------------------------------
@@ -117,6 +121,4 @@ pane.filebrowser.exit:
 
 
 txt.volume:
-        stri  'Volume:            Files:       Device: '
-txt.filelist:
-        stri  'Filename    Type   Size     Filename    Type   Size     Filename    Type   Size'       
+        stri  'Volume:            Files:      Device: '
