79491b4c88a1f681b92648e93672fe36d513f5b8
Filip van Vooren <filip.van-vooren@oratronik.de>
2024-12-27 22:47:44 +0100

    Bugfix: Prevent padding runaway

--- src/modules/pane.utils.hint.asm
+++ src/modules/pane.utils.hint.asm
@@ -38,17 +38,33 @@ pane.show_hintx:
         ;-------------------------------------------------------
         ; Get number of bytes to fill ...
         ;-------------------------------------------------------
-        mov   @parm2,tmp0
-        movb  *tmp0,tmp0            ; Get length byte of hint
-        srl   tmp0,8                ; Right justify
-        mov   tmp0,tmp2
+        mov   @parm2,tmp0           ; \
+        movb  *tmp0,tmp0            ; | Get length byte of hint
+        srl   tmp0,8                ; / MSB to LSB
+        ;-------------------------------------------------------
+        ; Skip padding if source string longer than pad length
+        ;-------------------------------------------------------
+        c     tmp0,@parm3           ;  String too long?
+        jgt   pane.show_hintx.exit  ;  Yes, exit
+        jeq   pane.show_hintx.exit  ;  Yes, exit
+        ;-------------------------------------------------------
+        ; Prepare for padding
+        ;-------------------------------------------------------
         mov   tmp0,tmp3             ; Work copy
-        neg   tmp2
-        a     @parm3,tmp2           ; Number of bytes to fill
+        mov   tmp0,tmp2             ; \
+        neg   tmp2                  ; | Calc number of bytes 
+        a     @parm3,tmp2           ; / to fill
+        ;------------------------------------------------------
+        ; Assert
+        ;------------------------------------------------------
+        ci    tmp2,80               ; Pad length bogus?
+        jle   !                     ; No, is fine. Continue
+        mov   r11,@>ffce            ; \ Save caller address
+        bl    @cpu.crash            ; / Crash and halt system
         ;-------------------------------------------------------
         ; ... and clear until end of line
         ;-------------------------------------------------------
-        mov   @parm1,tmp0           ; \ Restore YX position
+!       mov   @parm1,tmp0           ; \ Restore YX position
         a     tmp3,tmp0             ; | Adjust X position to end of string
         mov   tmp0,@wyx             ; / Set cursor
 
