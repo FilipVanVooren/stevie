3ad6488c191f361bcfa2bd69b460d9caef9e0561
Filip van Vooren <filip.van-vooren@oratronik.de>
2026-08-16 19:09:48 +0200

    Freeing space in bank 3

--- src/modules/rom.stubs.bank4.asm
+++ src/modules/rom.stubs.bank4.asm
@@ -151,6 +151,26 @@ vdp.colors.line:
 
 
 ***************************************************************
+* Stub for "vdp.cursor.tat.fb"
+* bank6 vec.4
+********|*****|*********************|**************************
+vdp.cursor.tat.fb:
+        dect  stack
+        mov   r11,*stack            ; Save return address
+        ;------------------------------------------------------
+        ; Call function in bank 6
+        ;------------------------------------------------------
+        bl    @rom.farjump          ; \ Trampoline jump to bank
+              data bank6.rom        ; | i  p0 = bank address
+              data vec.4            ; | i  p1 = Vector with target address
+              data bankid           ; / i  p2 = Source ROM bank for return
+        ;------------------------------------------------------
+        ; Exit
+        ;------------------------------------------------------
+        mov   *stack+,r11           ; Pop r11
+        b     *r11                  ; Return to caller
+
+***************************************************************
 * Stub for "vdp.cursor.tat"
 * bank6 vec.5
 ********|*****|*********************|**************************
