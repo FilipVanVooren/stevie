184e33f29ad78c9a1cf58f61d1c8a0ec7735483c
Filip van Vooren <filip.van-vooren@oratronik.de>
2026-08-31 18:56:49 +0200

    Added function-table with fingerprints of all functions in Stevie

--- src/modules/pane.botline.keycolor.asm
+++ src/modules/pane.botline.keycolor.asm
@@ -15,7 +15,7 @@
 *--------------------------------------------------------------
 * Register usage
 * tmp0 = pointer to keycolors array
-* tmp1 = Byte array byte
+* tmp1 = Color combination bottom bar (QR) / Byte array byte
 * tmp2 = Repeat count
 * tmp3 = Color combination for key marker (ST)
 * tmp4 = Copy of tmp1
@@ -39,10 +39,28 @@ pane.botline.keycolor:
         ; This fills @outparm1..@outparm5 where outparm5 contains QRST
         ;------------------------------------------------------
         bl    @pane.colorscheme.index 
-                                    ; \ Compose final TAT byte from QRST: 
-                                    ; / FG=S (high nibble of low byte), BG=T (low nibble)
-        mov   @outparm5,tmp3        ; \ tmp3 = QRST
-        andi  tmp3,>00ff            ; / Only keep ST
+                                    ; \ Compose final TAT bytes from QRST: 
+                                    ; | Normal Colors: FG=Q (high nibble of high byte), BG=R (low nibble of high byte)
+                                    ; / Key Colorso: FG=S (high nibble of low byte), BG=T (low nibble of low byte)
+        
+        mov   @outparm5,tmp1        ; tmp1 = QRST
+        srl   tmp1,8                ; QR to low byte
+        ;------------------------------------------------------
+        ; Fill RAMBUF with normal bottom-line color combination
+        ;------------------------------------------------------
+        li    tmp0,rambuf           ; Address of RAM work buffer
+        mov   tmp4,tmp1             ; Fill byte = default bottom-line color
+        li    tmp2,80               ; Bottom line is 80 chars wide
+        bl    @xfilm                ; Fill RAM buffer with normal color
+                                    ; i \  tmp0 = RAM start address
+                                    ; i |  tmp1 = byte to fill
+                                    ; i /  tmp2 = number of bytes to fill
+        ;------------------------------------------------------
+        ; Prepare for settings key colors
+        ;------------------------------------------------------
+        mov   @cmdb.keycolors,tmp0  ; Reset pointer to keycolor list
+        mov   @outparm5,tmp3        ; tmp3 = QRST
+        sla   tmp3,8                ; Move ST to high byte
         ;------------------------------------------------------
         ; Loop through array of (X,repeat) pairs
         ; Each entry: X pos (byte), repeat count (byte). Terminator: >ff
@@ -51,42 +69,32 @@ pane.botline.keycolor.loop:
         movb  *tmp0+,tmp1           ; Read X position (byte)
         srl   tmp1,8                ; Move to LSB
         ci    tmp1,>ff              ; Check sentinel
-        jeq   pane.botline.keycolor.prexit
-        mov   tmp1,tmp4             ; Backup tmp1
+        jeq   pane.botline.keycolor.dump
+        mov   tmp1,tmp4             ; Backup x-position
         movb  *tmp0+,tmp2           ; Read repeat count (byte)
         srl   tmp2,8                ; Move to LSB (repeat count)
         ;------------------------------------------------------
-        ; Dump color to TAT
+        ; Apply custom key color to RAMBUF
         ;------------------------------------------------------
-pane.botline.keycolor.dump.tat:        
-        dect  stack
-        mov   tmp0,*stack           ; Push tmp0
-        dect  stack
-        mov   tmp1,*stack           ; Push tmp1
-        dect  stack
-        mov   tmp2,*stack           ; Push tmp2
-
-        li    tmp0,vdp.botrow.tat   ; \ Get TAT address of bottom row
-        a     tmp4,tmp0             ; / Add X position
-
-        mov   tmp3,tmp1             ; Get color combination
-        mov   *stack,tmp2           ; Get repeat count 
-
-        bl    @xfilv                ; Fill colors
-                                    ; i \  tmp0 = start address
-                                    ; i |  tmp1 = byte to fill
-                                    ; i /  tmp2 = number of bytes to fill
-
-        mov   *stack+,tmp2          ; Pop tmp2
-        mov   *stack+,tmp1          ; Pop tmp1
-        mov   *stack+,tmp0          ; Pop tmp0
-
+pane.botline.keycolor.apply:
+        movb  tmp3,@rambuf(tmp4)    ; Set color byte in work buffer
+        inc   tmp4                  ; Next column
+        dec   tmp2                  ; Repeat count--
+        jne   pane.botline.keycolor.apply
         jmp   pane.botline.keycolor.loop
+        ;------------------------------------------------------
+        ; Copy the processed 80-char RAM buffer to VDP TAT area
+        ;------------------------------------------------------
+pane.botline.keycolor.dump:
+        bl    @cpym2v               ; \ Copy 80-byte RAM buffer to TAT
+              data vdp.botrow.tat   ; | i  p1 = destination VDP address
+              data rambuf           ; | i  p2 = source RAM address
+              data 80               ; / i  p3 = number of bytes to copy
         ;------------------------------------------------------
         ; Restore current SAMS pages
         ;------------------------------------------------------
-pane.botline.keycolor.prexit:        
-        bl    @mem.sams.dialogs.off ; Turn off SAMS pages #2 (>b000) and #3 (>c000)        
+pane.botline.keycolor.prexit:
+        bl    @mem.sams.dialogs.off ; Turn off SAMS pages #2 (>b000) and #3 (>c000)
         ;------------------------------------------------------
         ; Exit
         ;------------------------------------------------------
